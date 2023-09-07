ed76d4afa0d1aab606989404d137c83c3fd627ba
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-07 12:54:02 -0300

    refactor: adding weapon type

---
+++ src/bot/type/weapons/Weapon.ts
@@ -0,0 +1,14 @@
+import type Axe from './Axe'
+import type BastardSword from './BastardSword'
+import type BattleAxe from './BattleAxe'
+import type Bow from './Bow'
+import type Crossbow from './Crossbow'
+import type Dagger from './Dagger'
+import type Hammer from './Hammer'
+import type Spear from './Spear'
+import type Staff from './Staff'
+import type Sword from './Sword'
+
+type Weapon = Axe | BastardSword | BattleAxe | Bow | Crossbow | Dagger | Hammer | Spear | Staff | Sword
+
+export default Weapon
