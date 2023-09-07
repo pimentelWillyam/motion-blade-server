6050d7214ff86b279daeb12b9771745012146102
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-07 12:55:02 -0300

    feat: adding armor

---
+++ src/bot/type/armors/Armor.ts
@@ -0,0 +1,9 @@
+import type Chainmail from './Chainmail'
+import type Cloth from './Cloth'
+import type Leather from './Leather'
+import type Plate from './Plate'
+import type Pleather from './Pleather'
+
+type Armour = Cloth | Leather | Chainmail | Plate | Pleather
+
+export default Armour
