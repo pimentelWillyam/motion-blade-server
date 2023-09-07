d4f3a0da7c790670181cce94969abb255dfc00ce
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-07 12:27:23 -0300

    feat: adding bastard sword

---
+++ src/bot/type/weapons/BastardSword.ts
@@ -0,0 +1,10 @@
+interface BastardSword {
+  name: 'bastard sword'
+  maestry: 'two handed'
+  damageType: 'cutting'
+  strikable: true
+  throwable: false
+  shootable: false
+}
+
+export default BastardSword
