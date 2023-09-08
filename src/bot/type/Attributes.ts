3f6b0af88ecc5926e15688e195cf0a768474774c
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-08 15:05:34 -0300

    feat: adding buff and debuff to attributes

--- src/bot/type/Attributes.ts
+++ src/bot/type/Attributes.ts
@@ -1,8 +1,11 @@
 type Attributes = {
-  agility: number,
-  technique: number,
-  strength: number,
+  agility: number
+  technique: number
+  strength: number
   fortitude: number
+  guard: number
+  buff: number
+  debuff: number
 }
 
 export default Attributes
