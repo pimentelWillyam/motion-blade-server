4dab64da5cae2f5dc31aba869b82a1ea3d5a33d3
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-08 16:17:00 -0300

    feat: adding armor fetcher class

---
+++ src/bot/fetchers/ArmorFetcher.ts
@@ -0,0 +1,20 @@
+import type Cloth from '../model/armors/Cloth'
+import type Leather from '../model/armors/Leather'
+import type Chainmail from '../model/armors/Chainmail'
+import type Plate from '../model/armors/Plate'
+import type Pleather from '../model/armors/Pleather'
+import type Armor from '../type/Armor'
+
+class ArmorFetcher {
+  constructor (private readonly cloth: Cloth, private readonly leather: Leather, private readonly chainmail: Chainmail, private readonly plate: Plate, private readonly pleather: Pleather) {}
+
+  fetchArmorBasedOnFortitude = (fortitude: number): Armor => {
+    if (fortitude >= this.pleather.minimumFortitude) return this.pleather
+    else if (fortitude > this.plate.minimumFortitude) return this.plate
+    else if (fortitude > this.chainmail.minimumFortitude) return this.chainmail
+    else if (fortitude > this.leather.minimumFortitude) return this.leather
+    else return this.cloth
+  }
+}
+
+export default ArmorFetcher
