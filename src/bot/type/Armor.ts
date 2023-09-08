38c52d4ad5d7fba423453b5daf1c521a091526d1
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-08 14:51:03 -0300

    feat: adding armor type

---
+++ src/bot/type/Armor.ts
@@ -0,0 +1,9 @@
+import type Chainmail from '../model/armors/Chainmail'
+import type Cloth from '../model/armors/Cloth'
+import type Leather from '../model/armors/Leather'
+import type Plate from '../model/armors/Plate'
+import type Pleather from '../model/armors/Pleather'
+
+type Armor = Chainmail | Cloth | Leather | Plate | Pleather
+
+export default Armor
