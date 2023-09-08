b50d460816c7e73dd918553a658ae77cd63884c7
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-08 14:54:59 -0300

    feat: adding weapon type

---
+++ src/bot/type/WeaponType.ts
@@ -0,0 +1,4 @@
+type WeaponType = 'machado' | 'mão nua' | 'espada bastarda' | 'machado de batalha' | 'arco' | 'besta' | 'adaga' | 'martelo' | 'maça' | 'lança' | 'bastão' | 'espada'
+| 'sword'
+
+export default WeaponType
