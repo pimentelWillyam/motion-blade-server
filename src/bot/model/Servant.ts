9103e084fcf32cb723b2d7fc7e7911b706374355
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-09 00:41:46 -0300

    refactor: adding weaponFetcher

--- src/bot/model/Servant.ts
+++ src/bot/model/Servant.ts
@@ -1,11 +1,11 @@
 import type Armor from '../type/Armor'
 import type Weapon from '../type/Weapon'
-import BareHand from './weapons/BareHand'
 import type Profession from '../type/Profession'
 import type Attributes from '../type/Attributes'
 import type ArmorFetcher from '../fetchers/ArmorFetcher'
 import type AttributesFetcher from '../fetchers/AttributesFetcher'
 import type Maestry from '../type/Maestry'
+import type WeaponFetcher from '../fetchers/WeaponFetcher';
 
 class Servant {
   readonly id: string
@@ -20,13 +20,13 @@ class Servant {
   currentWeapon: Weapon
   maestry: Maestry
 
-  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, private readonly attributesFetcher: AttributesFetcher, private readonly armorFetcher: ArmorFetcher) {
+  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, private readonly weaponFetcher: WeaponFetcher, private readonly attributesFetcher: AttributesFetcher, private readonly armorFetcher: ArmorFetcher) {
     this.id = id
     this.masterId = masterId
     this.name = name
     this.fatherProfession = fatherProfession
     this.youthProfession = youthProfession
-    this.weaponList = [new BareHand(), new BareHand(), new BareHand()]
+    this.weaponList = [weaponFetcher.fetchWeaponByType('mão nua'), weaponFetcher.fetchWeaponByType('mão nua'), weaponFetcher.fetchWeaponByType('mão nua')]
     this.currentWeapon = this.weaponList[0]
     this.attributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
     this.fullHealth = this.attributes
