322701b0ce191e0de31111d6a2f9e36d13acac7c
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-09 00:27:10 -0300

    feat: adding new professions

--- src/bot/type/Profession.ts
+++ src/bot/type/Profession.ts
@@ -1,4 +1,4 @@
-type Profession = 'soldado' | 'bandido' | 'ferreiro' | 'bárbaro' | 'açougueiro' | 'lenhador' | 'guerreiro' | 'batedor' | 'saqueador' | 'assassino' | 'ladrão' | 'nômade' | 'monge'
-| 'infante' | 'patrulheiro' | 'nobre' | 'escravo' | 'arqueiro' | 'acrobata' | 'malabarista' | 'caçador'
+type Profession = 'soldado' | 'bandido' | 'ferreiro' | 'barbaro' | 'açougueiro' | 'lenhador' | 'guerreiro' | 'batedor' | 'saqueador' | 'assassino' | 'ladrao' | 'nomade' | 'monge'
+| 'infante' | 'patrulheiro' | 'nobre' | 'escravo' | 'arqueiro' | 'acrobata' | 'cavaleiro' | 'malabarista' | 'caçador'
 
 export default Profession
