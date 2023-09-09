176730b44b187fd1dcc73fe1b57c3b8a84d089f2
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-09 00:26:24 -0300

    feat: adding servant class

---
+++ src/bot/model/Servant.ts
@@ -0,0 +1,38 @@
+import type Armor from '../type/Armor'
+import type Weapon from '../type/Weapon'
+import BareHand from './weapons/BareHand'
+import type Profession from '../type/Profession'
+import type Attributes from '../type/Attributes'
+import type ArmorFetcher from '../fetchers/ArmorFetcher'
+import type AttributesFetcher from '../fetchers/AttributesFetcher'
+import type Maestry from '../type/Maestry'
+
+class Servant {
+  readonly id: string
+  readonly masterId: string
+  readonly name: string
+  readonly fatherProfession: Profession
+  readonly youthProfession: Profession
+  attributes: Attributes
+  fullHealth: Attributes
+  armor: Armor
+  weaponList: Weapon[]
+  currentWeapon: Weapon
+  maestry: Maestry
+
+  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, private readonly attributesFetcher: AttributesFetcher, private readonly armorFetcher: ArmorFetcher) {
+    this.id = id
+    this.masterId = masterId
+    this.name = name
+    this.fatherProfession = fatherProfession
+    this.youthProfession = youthProfession
+    this.weaponList = [new BareHand(), new BareHand(), new BareHand()]
+    this.currentWeapon = this.weaponList[0]
+    this.attributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
+    this.fullHealth = this.attributes
+    this.armor = this.armorFetcher.fetchArmorBasedOnFortitude(this.attributes.fortitude)
+    this.maestry = { bow: 0, crossbow: 0, nakedHanded: 0, oneHanded: 0, twoHanded: 0, polearm: 0 }
+  }
+}
+
+export default Servant
