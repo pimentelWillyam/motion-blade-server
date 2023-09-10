e5c841164cb8dad2fb3a0fe17cd8c281166e2467
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-10 18:30:49 -0300

    feat: adding memory data source

--- src/data/memory/MemoryDataSource.ts
+++ src/data/memory/MemoryDataSource.ts
@@ -1,27 +1,64 @@
-import type Master from '../../bot/model/Master'
-import type Servant from '../../bot/model/Servant'
-import type IMemoryDataSource from './IMemoryDataSource'
+import type AttributesFetcher from '../../bot/fetchers/AttributesFetcher'
+import Servant from '../../bot/model/Servant'
+import type Profession from '../../bot/type/Profession'
+import type WeaponFetcher from '../../bot/fetchers/WeaponFetcher'
+import type ArmorFetcher from '../../bot/fetchers/ArmorFetcher'
+import type UuidGenerator from '../../bot/helper/UuidGenerator'
 
-class MemoryDataSource implements IMemoryDataSource {
+class MemoryDataSource {
   private servantList: Servant[] = []
-  private masterList: Master[] = []
 
-  createMasterTable = (): void => {
-    this.masterList = []
+  constructor (private readonly uuidGenerator: UuidGenerator, private readonly weaponFetcher: WeaponFetcher, private readonly attributesFetcher: AttributesFetcher, private readonly armorFetcher: ArmorFetcher) {}
+
+  readonly start = (): void => {
+    this.createServantTable()
   }
 
-  createServantTable = (): void => {
+  private readonly createServantTable = (): void => {
     this.servantList = []
   }
 
-  insertMasterRegistry = (id: string, name: string, servantsOwnedIdList: string[]): void => {
-    this.masterList.push({ id, name, servantsOwnedIdList })
+  insertServantRegistry = (masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession): Servant => {
+    const servant: Servant = new Servant(this.uuidGenerator.generate(), masterId, name, fatherProfession, youthProfession, this.weaponFetcher, this.attributesFetcher, this.armorFetcher)
+    this.servantList.push(servant)
+    return servant
   }
 
-  insertServantRegistry = (id: string, name: string, servantsOwnedIdList: string[]): void => {
-    const servant: Servant
-    servant
-      .this.masterList.push({ id, name, servantsOwnedIdList })
+  fetchEveryServantRegistry = (): Servant[] => {
+    return this.servantList
+  }
+
+  fetchServantByName = (name: string): Servant | null => {
+    for (let i = 0; i < this.servantList.length; i++) {
+      if (this.servantList[i].name === name) return this.servantList[i]
+    }
+    return null
+  }
+
+  updateServantByName = (name: string, contentToUpdate: Servant): Servant => {
+    for (let i = 0; i < this.servantList.length; i++) {
+      if (this.servantList[i].name === name) {
+        if (contentToUpdate.weaponList !== undefined) this.servantList[i].weaponList = contentToUpdate.weaponList
+        if (contentToUpdate.currentWeapon !== undefined) this.servantList[i].currentWeapon = contentToUpdate.currentWeapon
+        if (contentToUpdate.armor !== undefined) this.servantList[i].armor = contentToUpdate.armor
+        if (contentToUpdate.currentAttributes !== undefined) this.servantList[i].currentAttributes = contentToUpdate.currentAttributes
+        if (contentToUpdate.maximumAttributes !== undefined) this.servantList[i].maximumAttributes = contentToUpdate.maximumAttributes
+        if (contentToUpdate.maestry !== undefined) this.servantList[i].maestry = contentToUpdate.maestry
+        return this.servantList[i]
+      }
+    }
+    throw new Error('Servo não encontrado')
+  }
+
+  deleteServantByName = (name: string): Servant => {
+    for (let i = 0; i < this.servantList.length; i++) {
+      if (this.servantList[i].name === name) {
+        const deletedServer = this.servantList[i]
+        this.servantList.splice(i, 1)
+        return deletedServer
+      }
+    }
+    throw new Error('Servo não encontrado')
   }
 }
 
