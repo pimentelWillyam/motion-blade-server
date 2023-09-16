2596f9303fa522e1d20a00ef2feadc1308337313
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-16 16:00:17 -0300

    refactor: adding custom creation parameters to servant model

--- src/bot/model/Servant.ts
+++ src/bot/model/Servant.ts
@@ -20,7 +20,7 @@ class Servant {
   currentWeapon: Weapon
   maestry: Maestry
 
-  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, private readonly weaponFetcher: WeaponFetcher, private readonly attributesFetcher: AttributesFetcher, private readonly armorFetcher: ArmorFetcher) {
+  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, private readonly weaponFetcher: WeaponFetcher, private readonly attributesFetcher: AttributesFetcher, private readonly armorFetcher: ArmorFetcher, customAttributesCreation = false, agility: number = 0, technique = 0, strength: number = 0, fortitude: number = 0) {
     this.id = id
     this.masterId = masterId
     this.name = name
@@ -28,8 +28,14 @@ class Servant {
     this.youthProfession = youthProfession
     this.inventory = []
     this.currentWeapon = this.weaponFetcher.fetchWeaponByType('mão nua')
-    this.currentAttributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
-    this.maximumAttributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
+    if (customAttributesCreation) {
+      this.currentAttributes = { agility, technique, strength, fortitude, guard: 0, buff: 0, debuff: 0 }
+      this.maximumAttributes = { agility, technique, strength, fortitude, guard: 0, buff: 0, debuff: 0 }
+    } else {
+      this.currentAttributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
+      this.maximumAttributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
+    }
+
     this.armor = this.armorFetcher.fetchArmorBasedOnFortitude(this.currentAttributes.fortitude)
     this.maestry = { bow: 0, crossbow: 0, bareHanded: 0, oneHanded: 0, twoHanded: 0, polearm: 0 }
   }
