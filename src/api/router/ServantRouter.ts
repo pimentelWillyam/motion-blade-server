9391c6e405ced5a0431dd1b9d5c8933cb92df7fd
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-20 21:25:05 -0300

    feat: adding servant routes

---
+++ src/api/router/ServantRouter.ts
@@ -0,0 +1,26 @@
+// importando core da rota
+import * as express from 'express'
+import { type Router, type Request, type Response } from 'express'
+import type ServantController from '../controller/ServantController'
+
+// importando service da rota
+
+// criando rotas
+
+class ServantRouter {
+  readonly routes: Router
+  constructor (readonly servantController: ServantController) {
+    this.routes = express.Router()
+    this.routes.post('/servant', (req: Request, res: Response) => {
+      void servantController.create(req, res)
+    })
+    this.routes.get('/servant', (req: Request, res: Response) => {
+      void servantController.getAll(res)
+    })
+    this.routes.get('/servant/:id', (req: Request, res: Response) => {
+      void servantController.get(req, res)
+    })
+  }
+}
+
+export default ServantRouter
