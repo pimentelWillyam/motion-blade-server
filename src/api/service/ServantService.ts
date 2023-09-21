bb77aa593eb35eb33d358e20bb0d589acbbbdbec
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-20 21:25:18 -0300

    refactor: importing types

--- src/api/service/ServantService.ts
+++ src/api/service/ServantService.ts
@@ -1,3 +1,9 @@
+import type UuidGenerator from '../../bot/helper/UuidGenerator'
+import type Servant from '../../bot/model/Servant'
+import type Profession from '../../bot/type/Profession'
+import type DateManager from '../../helper/DateManager'
+import type ServantRepository from '../repository/ServantRepository'
+
 class ServantService {
   constructor (readonly ServantRepository: IServantRepository, readonly uuidGenerator: IUuidGenerator, readonly dateManager: IDateManager) {}
 
