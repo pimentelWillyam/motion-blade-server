381ed509273426bc08c1a46d6fba8b8dde70c603
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-22 20:36:27 -0300

    refactor: fixing api imports

--- src/helper/Api.ts
+++ src/helper/Api.ts
@@ -1,6 +1,4 @@
-import type IApiMiddleware from '../api/interface/IApiMiddleware'
 import { type Express } from 'express'
-import type ILogRouter from '../api/interface/ILogRouter'
 
 import * as bodyParser from 'body-parser'
 import * as cors from 'cors'
@@ -8,11 +6,10 @@ import { json } from 'express'
 import type ServantRouter from '../api/router/ServantRouter'
 
 class Api {
-  constructor (readonly server: Express, readonly apiMiddleware: IApiMiddleware, readonly logRouter: ILogRouter, readonly servantRouter: ServantRouter) {
+  constructor (readonly server: Express, readonly servantRouter: ServantRouter) {
     this.server.use(bodyParser.json())
     this.server.use(json())
     this.server.use(cors())
-    this.server.use('/api', logRouter.routes)
     this.server.use('/api', servantRouter.routes)
   }
 }
