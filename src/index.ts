98553f75f5b1b7d5c304dac550cd2d2bcd866e78
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-22 20:37:04 -0300

    refactor: fixing index imports

--- src/index.ts
+++ src/index.ts
@@ -27,27 +27,21 @@ import * as express from 'express'
 
 // importing helpers
 import Api from './helper/Api'
-import ApiMiddleware from './helper/ApiMiddleware'
 import DateManager from './helper/DateManager'
 
 // importing data classes
-import MariadbDataSource from './data/MariadbDataSource'
+// import MariadbDataSource from './data/MariadbDataSource'
 // importing routers
-import LogRouter from './api/router/LogRouter'
 
 // importing controllers
-import LogController from './api/controller/LogController'
 import ServantController from './api/controller/ServantController'
 
 // importing validators
-import LogValidator from './api/validator/LogValidator'
 
 // importing services
-import LogService from './api/service/LogService'
 import ServantService from './service/ServantService'
 
 // importing repositories
-import LogRepository from './api/repository/LogRepository'
 
 // importing entities
 // import FileEntity from './api/entity/FileEntity'
@@ -56,7 +50,6 @@ import LogRepository from './api/repository/LogRepository'
 
 // importing app
 import App from './api/App'
-import DatabaseHelper from './helper/DatabaseHelper'
 import MemoryDataSource from './data/MemoryDataSource'
 import ServantRouter from './api/router/ServantRouter'
 import ServantValidator from './api/validator/ServantValidator'
@@ -88,7 +81,6 @@ const servantRepository = new ServantRepository(memoryDataSource)
 const servantService = new ServantService(servantRepository, attributesFetcher, armorFactory, weaponFactory)
 
 // instanciating validators
-const logValidator = new LogValidator()
 const servantValidator = new ServantValidator()
 
 // instanciating the servant controller
@@ -103,39 +95,34 @@ const messageHandler = new MessageHandler(commandManager)
 const discordBot = new DiscordBot(messageHandler)
 
 // instanciating helpers
-const dateManager = new DateManager()
 const server = new Server()
-const apiMiddleware = new ApiMiddleware()
+// const apiMiddleware = new ApiMiddleware()
 
 // instanciating data classes
-const databaseHelper = new DatabaseHelper()
-const mariadbDataSource = new MariadbDataSource(databaseHelper)
+// const databaseHelper = new DatabaseHelper()
+// const mariadbDataSource = new MariadbDataSource(databaseHelper)
 // const oracledbDataSource = new OracledbDataSource()
 
 // instanciating repositories
 // using repositories with mariadb
-const logRepository = new LogRepository(mariadbDataSource, uuidGenerator, dateManager)
 // const servantRepository = new ServantRepository(memoryDataSource)
 
 // instanciating services
-const logService = new LogService(logRepository, uuidGenerator, dateManager)
 
 // instanciating controllers
-const logController = new LogController(logService, logValidator)
 
 // instanciating routers
-const logRouter = new LogRouter(logController)
 const servantRouter = new ServantRouter(servantController)
 
 // instanciating app related classes
-const api = new Api(express(), apiMiddleware, logRouter, servantRouter)
+const api = new Api(express(), servantRouter)
 const app = new App(api, server)
 
 // getting .env configuration
 dotenv.config()
 
 // starting database and app
-void mariadbDataSource.openConnectionPool()
+// void mariadbDataSource.openConnectionPool()
 app.start()
 
 discordBot.client.start()
