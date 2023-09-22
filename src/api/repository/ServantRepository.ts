00f911e80de764014a99b45fb0dc4ed50242fa77
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-21 21:55:50 -0300

    refactor: removing useless method rom servant repository

--- src/api/repository/ServantRepository.ts
+++ src/api/repository/ServantRepository.ts
@@ -1,6 +1,6 @@
 import type Servant from '../../bot/model/Servant'
 import type Profession from '../../bot/type/Profession'
-import type MemoryDataSource from '../../data/memory/MemoryDataSource'
+import type MemoryDataSource from '../../data/MemoryDataSource'
 
 class ServantRepository {
   constructor (readonly dataSource: MemoryDataSource) {}
@@ -8,12 +8,6 @@ class ServantRepository {
   create (masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession): Servant {
     return this.dataSource.insertServantRegistry(masterId, name, fatherProfession, youthProfession)
   }
-
-  // async createCustomServant (masterId: string, id: string, name: string, fatherProfession: string, youthProfession: string): Promise<IServantEntity> {
-  //   const log = { id, date, message }
-  //   await this.dataSource.insertCustomAttributesServantRegistry(masterId, name, fatherProfession, youthProfession, agility, technique, strength, fortitude)
-  //   return log
-  // }
 
   getAll (): Servant[] {
     return this.dataSource.fetchEveryServantRegistry()
