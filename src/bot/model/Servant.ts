d5a834062358ff3fc9849e9741ea70d2373838b9
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-21 21:59:09 -0300

    refactor: reorganazing servant class

--- src/bot/model/Servant.ts
+++ src/bot/model/Servant.ts
@@ -2,10 +2,8 @@ import type Armor from '../type/Armor'
 import type Weapon from '../type/Weapon'
 import type Profession from '../type/Profession'
 import type Attributes from '../type/Attributes'
-import type ArmorFetcher from '../fetchers/ArmorFetcher'
-import type AttributesFetcher from '../fetchers/AttributesFetcher'
 import type Maestry from '../type/Maestry'
-import type WeaponFetcher from '../fetchers/WeaponFetcher'
+import type Inventory from '../type/Inventory'
 
 class Servant {
   readonly id: string
@@ -15,28 +13,24 @@ class Servant {
   readonly youthProfession: Profession
   currentAttributes: Attributes
   maximumAttributes: Attributes
-  armor: Armor
-  inventory: Weapon[]
-  currentWeapon: Weapon
+  guard: number
+  buff: number
+  debuff: number
+  inventory: Inventory
   maestry: Maestry
 
-  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, private readonly weaponFetcher: WeaponFetcher, private readonly attributesFetcher: AttributesFetcher, private readonly armorFetcher: ArmorFetcher, customAttributesCreation = false, agility: number = 0, technique = 0, strength: number = 0, fortitude: number = 0) {
+  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, armor: Armor, currentWeapon: Weapon, attributes: Attributes = { agility: 0, technique: 0, strength: 0, fortitude: 0 }) {
     this.id = id
     this.masterId = masterId
     this.name = name
     this.fatherProfession = fatherProfession
     this.youthProfession = youthProfession
-    this.inventory = []
-    this.currentWeapon = this.weaponFetcher.fetchWeaponByType('mão nua')
-    if (customAttributesCreation) {
-      this.currentAttributes = { agility, technique, strength, fortitude, guard: 0, buff: 0, debuff: 0 }
-      this.maximumAttributes = { agility, technique, strength, fortitude, guard: 0, buff: 0, debuff: 0 }
-    } else {
-      this.currentAttributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
-      this.maximumAttributes = this.attributesFetcher.fetchAttributesBasedOnBackground(fatherProfession, youthProfession)
-    }
-
-    this.armor = this.armorFetcher.fetchArmorBasedOnFortitude(this.currentAttributes.fortitude)
+    this.currentAttributes = attributes
+    this.maximumAttributes = attributes
+    this.guard = 0
+    this.buff = 0
+    this.debuff = 0
+    this.inventory = { armor, carriedWeapons: [], currentWeapon, denars: 0 }
     this.maestry = { bow: 0, crossbow: 0, bareHanded: 0, oneHanded: 0, twoHanded: 0, polearm: 0 }
   }
 }
