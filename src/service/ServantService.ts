e7b6906b6f778e8db5fb99ac7def163d9b257313
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-27 13:00:20 -0300

    refactor: removing useless methods

--- src/service/ServantService.ts
+++ src/service/ServantService.ts
@@ -9,7 +9,6 @@ import type Maestry from '../bot/type/Maestry'
 import type MaestryType from '../bot/type/MaestryType'
 import type Profession from '../bot/type/Profession'
 import type ServantLifeStatus from '../bot/type/ServantLifeStatus'
-import type Weapon from '../bot/type/Weapon'
 import type WeaponType from '../bot/type/WeaponType'
 import type { ArmorFactory } from '../factories/ArmorFactory'
 import type { Servant, ServantFactory } from '../factories/ServantFactory'
@@ -300,40 +299,6 @@ class ServantService {
   armorEvasionTest = (attacker: Servant, attackerDiceResult: number, defender: Servant, defenderDiceResult: number): ArmorEvasionTestResult => {
     if (attacker.currentAttributes.technique + attackerDiceResult > defender.currentAttributes.technique + defenderDiceResult) return 'Evaded armor'
     else return 'Hit armor'
-  }
-
-  getDamageToDeal (attacker: Servant, attackerDiceResult: number, attackerWeapon: Weapon, defender: Servant, defenderArmorType: ArmorType, defenderDiceResult: number): number {
-    let attackerTestResult = attacker.currentAttributes.strength + attackerDiceResult
-    const defenderTestResult = defender.currentAttributes.fortitude + defenderDiceResult
-    attackerTestResult += this.getAttackerMaestryFromWeaponUsed(attacker, attackerWeapon)
-    attackerTestResult += this.getWeaponArmourDamageRelation(attacker.inventory.currentWeapon, defenderArmorType)
-    return attackerTestResult - defenderTestResult
-  }
-
-  getAttackerMaestryFromWeaponUsed (attacker: Servant, weapon: Weapon): number {
-    if (weapon.type === 'mão nua') return attacker.maestry.bareHanded
-    else if (weapon.type === 'machado' || weapon.type === 'adaga' || weapon.type === 'martelo' || weapon.type === 'maça' || weapon.type === 'espada') return attacker.maestry.oneHanded
-    else if (weapon.type === 'espada bastarda' || weapon.type === 'machado de batalha') return attacker.maestry.twoHanded
-    else if (weapon.type === 'lança' || weapon.type === 'bastão') return attacker.maestry.polearm
-    else if (weapon.type === 'arco') return attacker.maestry.bow
-    else if (weapon.type === 'besta') return attacker.maestry.crossbow
-    else throw new Error('Tipo de arma inválido')
-  }
-
-  getWeaponArmourDamageRelation (weapon: Weapon, defenderArmorType: ArmorType): number {
-    if (weapon.damageType === 'corte' && defenderArmorType === 'roupa') return 6
-    if (weapon.damageType === 'corte' && defenderArmorType === 'couro') return 4
-    if (weapon.damageType === 'corte' && defenderArmorType === 'cota de malha') return 2
-    if (weapon.damageType === 'corte' && defenderArmorType === 'placa') return -3
-    if (weapon.damageType === 'perfuração' && defenderArmorType === 'roupa') return 6
-    if (weapon.damageType === 'perfuração' && defenderArmorType === 'couro') return 5
-    if (weapon.damageType === 'perfuração' && defenderArmorType === 'cota de malha') return -1
-    if (weapon.damageType === 'perfuração' && defenderArmorType === 'placa') return -1
-    if (weapon.damageType === 'impacto' && defenderArmorType === 'roupa') return 5
-    if (weapon.damageType === 'impacto' && defenderArmorType === 'couro') return 3
-    if (weapon.damageType === 'impacto' && defenderArmorType === 'cota de malha') return 1
-    if (weapon.damageType === 'impacto' && defenderArmorType === 'placa') return 0
-    else throw new Error('Tipo de dano inválido')
   }
 
   dealDamage = async (servant: Servant, damageToDeal: number): Promise<ServantLifeStatus> => {
