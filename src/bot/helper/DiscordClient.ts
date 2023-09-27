2468513f0c703cf33b0cca535709451aa8e674b1
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-27 12:34:00 -0300

    refactor: adding void return and removing interface

--- src/bot/helper/DiscordClient.ts
+++ src/bot/helper/DiscordClient.ts
@@ -1,15 +1,13 @@
-import type IMessageHandler from '../interface/IMessageHandler'
-
-import type IDiscordClient from '../interface/IDiscordClient'
 
 import { type Client, Events, type Message } from 'discord.js'
+import type MessageHandler from '../handler/MessageHandler'
 
-class DiscordClient implements IDiscordClient {
-  constructor (private readonly client: Client, private readonly messageHandler: IMessageHandler) {}
+class DiscordClient {
+  constructor (private readonly client: Client, private readonly messageHandler: MessageHandler) {}
 
   start (): void {
     console.log('Logging in...')
-    this.client.login(process.env.token)
+    void this.client.login(process.env.token)
     this.client.on('ready', (c) => {
       console.log(`✅ ${c.user.tag} is online.`)
     })
@@ -20,14 +18,14 @@ class DiscordClient implements IDiscordClient {
     console.log('Listening to messages...')
     this.client.on(Events.MessageCreate, (message: Message) => {
       try {
-        this.messageHandler.handle(message)
+        void this.messageHandler.handle(message)
       } catch (error) {
         if (error instanceof Error) {
           console.error(error)
-          message.reply(error.message)
+          void message.reply(error.message)
         } else {
           console.error(error)
-          message.reply('Aconteceu um erro inesperado')
+          void message.reply('Aconteceu um erro inesperado')
         }
       }
     })
