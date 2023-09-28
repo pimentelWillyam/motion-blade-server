7443902a8c56b8f84ed1c112ba64e788f7dc071a
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-28 13:03:11 -0300

    delete: removing testing

--- src/testing.ts
+++
@@ -1,7 +0,0 @@
-import DatabaseHelper from './helper/DatabaseHelper'
-import MariadbDataSource from './data/MariadbDataSource'
-
-const databaseHelper = new DatabaseHelper()
-const mariadb = new MariadbDataSource(databaseHelper)
-
-void mariadb.bootstrap()
