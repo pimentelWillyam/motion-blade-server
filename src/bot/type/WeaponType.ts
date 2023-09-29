cd950d3a31d6a4c45849902abe86b5be30af09a6
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-29 20:05:54 -0300

    refactor: adding new weapon type

--- src/bot/type/WeaponType.ts
+++ src/bot/type/WeaponType.ts
@@ -1,4 +1,5 @@
-type WeaponType = 'machado' | 'mão nua' | 'espada bastarda' | 'machado de batalha' | 'arco' | 'besta' | 'adaga' | 'martelo' | 'maça' | 'lança' | 'bastão' | 'espada'
-| 'sword'
+type WeaponType = 'mão nua' | 'faca' | 'adaga' | 'facão' | 'gládio' | 'sabre' | 'espada' | 'machadinha' | 'machado' | 'clava' | 'maça' | 'maça estrelada' | 'espada bastarda' |
+'katana' | 'claymore' | 'zweihander' | 'machado de batalha' | 'maça pesada' | 'martelo pesado' | 'lança' | 'lança montada' | 'falange' | 'alabarda' | 'arco curto' | 'arco' |
+'arco longo' | 'besta' | 'besta pesada' | 'escudo redondo' | 'escudo' | 'scutum'
 
 export default WeaponType
