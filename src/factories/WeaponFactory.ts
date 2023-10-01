19437f1f8d70fce80249a3155152fe8928c29d78
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-10-01 09:05:17 -0300

    refactor: adding new weapon creation

--- src/factories/WeaponFactory.ts
+++ src/factories/WeaponFactory.ts
@@ -5,35 +5,69 @@ import type Damage from '../bot/type/Damage'
 class Weapon {
   readonly type: WeaponType
   readonly maestryType: MaestryType
-  readonly damageType: Damage
   readonly strikable: boolean
   readonly throwable: boolean
   readonly shootable: boolean
+  readonly damage: Damage
+  readonly strikingBuff: number
+  readonly defendingBuff: number
 
-  constructor (type: WeaponType, maestryType: MaestryType, damageType: Damage, strikable: boolean, throwable: boolean, shootable: boolean) {
+  constructor (type: WeaponType, maestryType: MaestryType, strikable: boolean, throwable: boolean, shootable: boolean, damage: Damage, strikingBuff: number, defendingBuff: number) {
     this.type = type
     this.maestryType = maestryType
-    this.damageType = damageType
     this.strikable = strikable
     this.throwable = throwable
     this.shootable = shootable
+    this.damage = damage
+    this.strikingBuff = strikingBuff
+    this.defendingBuff = defendingBuff
   }
 }
 
 class WeaponFactory {
   createWeapon (weaponType: WeaponType): Weapon {
-    if (weaponType === 'adaga') return new Weapon(weaponType, 'uma mão', 'corte', true, true, false)
-    else if (weaponType === 'arco') return new Weapon(weaponType, 'arco', 'perfuração', false, false, true)
-    else if (weaponType === 'bastão') return new Weapon(weaponType, 'haste', 'impacto', true, false, false)
-    else if (weaponType === 'besta') return new Weapon(weaponType, 'besta', 'perfuração', false, false, true)
-    else if (weaponType === 'espada') return new Weapon(weaponType, 'uma mão', 'corte', true, false, false)
-    else if (weaponType === 'espada bastarda') return new Weapon(weaponType, 'duas mãos', 'corte', true, false, false)
-    else if (weaponType === 'lança') return new Weapon(weaponType, 'haste', 'perfuração', true, true, false)
-    else if (weaponType === 'machado') return new Weapon(weaponType, 'uma mão', 'corte', true, true, false)
-    else if (weaponType === 'machado de batalha') return new Weapon(weaponType, 'duas mãos', 'corte', true, false, false)
-    else if (weaponType === 'martelo') return new Weapon(weaponType, 'uma mão', 'impacto', true, false, false)
-    else if (weaponType === 'maça') return new Weapon(weaponType, 'uma mão', 'impacto', true, false, false)
-    else if (weaponType === 'mão nua') return new Weapon(weaponType, 'uma mão', 'impacto', true, false, false)
+    if (weaponType === 'mão nua') return new Weapon(weaponType, 'uma mão', true, true, false, { cloth: 2, leather: 0, chainmail: -6, plate: -10 }, 2, -5)
+
+    else if (weaponType === 'faca') return new Weapon(weaponType, 'uma mão', true, true, false, { cloth: 4, leather: 2, chainmail: -2, plate: -10 }, 4, -4)
+    else if (weaponType === 'adaga') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 5, leather: 3, chainmail: -1, plate: -7 }, 3, -3)
+    else if (weaponType === 'gládio') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 6, leather: 4, chainmail: 3, plate: -6 }, 0, -1)
+    else if (weaponType === 'facão') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 5, leather: 3, chainmail: -7, plate: -10 }, 1, -1)
+    else if (weaponType === 'sabre') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 6, leather: 4, chainmail: -4, plate: -10 }, 0, 0)
+    else if (weaponType === 'espada') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 5, leather: 4, chainmail: -3, plate: -9 }, 0, 0)
+
+    else if (weaponType === 'machadinha') return new Weapon(weaponType, 'uma mão', true, true, false, { cloth: 4, leather: 3, chainmail: -2, plate: -4 }, 1, -1)
+    else if (weaponType === 'machado') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 5, leather: 4, chainmail: -1, plate: -3 }, 0, 0)
+
+    else if (weaponType === 'clava') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 3, leather: 1, chainmail: 0, plate: 1 }, 0, 0)
+    else if (weaponType === 'maça') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 4, leather: 3, chainmail: 2, plate: 1 }, 0, 0)
+    else if (weaponType === 'maça estrelada') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 5, leather: 4, chainmail: 3, plate: 1 }, 0, 0)
+
+    else if (weaponType === 'espada bastarda') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 5, leather: 4, chainmail: -3, plate: -5 }, 0, 0)
+    else if (weaponType === 'katana') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 5, leather: 4, chainmail: -3, plate: -5 }, 3, 0)
+    else if (weaponType === 'claymore') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 7, leather: 5, chainmail: -2, plate: -1 }, -1, -1)
+    else if (weaponType === 'zweihander') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 8, leather: 6, chainmail: -1, plate: 0 }, -3, -3)
+
+    else if (weaponType === 'machado de batalha') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 7, leather: 6, chainmail: 1, plate: -1 }, -3, -3)
+
+    else if (weaponType === 'maça pesada') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 6, leather: 5, chainmail: 3, plate: 5 }, -3, -3)
+    else if (weaponType === 'martelo pesado') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 7, leather: 6, chainmail: 4, plate: 6 }, -5, -5)
+
+    else if (weaponType === 'bastao') return new Weapon(weaponType, 'uma mão', true, true, false, { cloth: 3, leather: 1, chainmail: -4, plate: -7 }, 3, -3)
+    else if (weaponType === 'lança') return new Weapon(weaponType, 'uma mão', true, true, false, { cloth: 6, leather: 4, chainmail: 3, plate: -6 }, 3, -3)
+    else if (weaponType === 'lança montada') return new Weapon(weaponType, 'uma mão', true, true, false, { cloth: 4, leather: 3, chainmail: 2, plate: 1 }, 3, -3)
+    else if (weaponType === 'sarissa') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 6, leather: 4, chainmail: 3, plate: -6 }, 5, -5)
+    else if (weaponType === 'alabarda') return new Weapon(weaponType, 'uma mão', true, false, false, { cloth: 7, leather: 4, chainmail: 3, plate: 2 }, 5, -5)
+
+    else if (weaponType === 'arco curto') return new Weapon(weaponType, 'uma mão', false, false, true, { cloth: 4, leather: 2, chainmail: -2, plate: -10 }, 3, -10)
+    else if (weaponType === 'arco') return new Weapon(weaponType, 'uma mão', false, false, true, { cloth: 5, leather: 3, chainmail: -1, plate: -7 }, 0, -10)
+    else if (weaponType === 'arco longo') return new Weapon(weaponType, 'uma mão', false, false, true, { cloth: 6, leather: 4, chainmail: 2, plate: -3 }, -5, -10)
+
+    else if (weaponType === 'besta') return new Weapon(weaponType, 'uma mão', false, false, true, { cloth: 4, leather: 2, chainmail: 0, plate: -3 }, 1, -10)
+    else if (weaponType === 'besta pesada') return new Weapon(weaponType, 'uma mão', false, false, true, { cloth: 6, leather: 5, chainmail: 4, plate: 2 }, -3, 10)
+
+    else if (weaponType === 'escudo redondo') return new Weapon(weaponType, 'uma mão', false, false, false, { cloth: 0, leather: 0, chainmail: 0, plate: 0 }, 0, 4)
+    else if (weaponType === 'escudo') return new Weapon(weaponType, 'uma mão', false, false, false, { cloth: 0, leather: 0, chainmail: 0, plate: 0 }, -1, 6)
+    else if (weaponType === 'scutum') return new Weapon(weaponType, 'uma mão', false, false, false, { cloth: 0, leather: 0, chainmail: 0, plate: 0 }, -3, 7)
     else throw new Error('Tipo de arma inválido')
   }
 }
