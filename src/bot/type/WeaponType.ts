347dc141e2872f277669e62098303667d95bd995
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-10-01 09:05:03 -0300

    refactor: adding sarissa

--- src/bot/type/WeaponType.ts
+++ src/bot/type/WeaponType.ts
@@ -1,5 +1,5 @@
 type WeaponType = 'mão nua' | 'faca' | 'adaga' | 'facão' | 'gládio' | 'sabre' | 'espada' | 'machadinha' | 'machado' | 'clava' | 'maça' | 'maça estrelada' | 'espada bastarda' |
-'katana' | 'claymore' | 'zweihander' | 'machado de batalha' | 'maça pesada' | 'martelo pesado' | 'bastao' | 'lança' | 'lança montada' | 'falange' | 'alabarda' | 'arco curto' |
+'katana' | 'claymore' | 'zweihander' | 'machado de batalha' | 'maça pesada' | 'martelo pesado' | 'bastao' | 'lança' | 'lança montada' | 'sarissa' | 'alabarda' | 'arco curto' |
 'arco' | 'arco longo' | 'besta' | 'besta pesada' | 'escudo redondo' | 'escudo' | 'scutum'
 
 export default WeaponType
