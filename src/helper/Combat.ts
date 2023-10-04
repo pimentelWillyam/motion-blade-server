4813e3c5158332ca9b1a10f2976ab0728c64a0b7
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-10-03 22:44:00 -0300

    refactor: adding necessary imports

--- src/helper/Combat.ts
+++ src/helper/Combat.ts
@@ -1,3 +1,6 @@
+import { type Servant } from '../factories/ServantFactory'
+import type RandomNumberGenerator from '../bot/helper/RandomNumberGenerator'
+
 class Combat {
   constructor (private readonly randomNumberGenerator: RandomNumberGenerator) {}
 
