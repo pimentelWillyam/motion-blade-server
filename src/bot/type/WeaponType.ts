ea04ec2723891cd47e2511f56e369652e06901dc
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-10-13 23:44:54 -0300

    Update WeaponType.ts

--- src/bot/type/WeaponType.ts
+++ src/bot/type/WeaponType.ts
@@ -1,4 +1,4 @@
-type WeaponType = 'mão nua' | 'faca' | 'adaga' | 'facão' | 'gládio' | 'sabre' | 'espada' | 'machadinha' | 'machado' | 'clava' | 'maça' | 'maça estrelada' | 'espada bastarda' |
+type WeaponType = 'mão nua' | 'faca' | 'adaga' | 'facão' | 'gladio' | 'sabre' | 'espada' | 'machadinha' | 'machado' | 'clava' | 'maça' | 'maça estrelada' | 'espada bastarda' |
 'katana' | 'claymore' | 'zweihander' | 'machado de batalha' | 'maça pesada' | 'martelo pesado' | 'bastao' | 'lança' | 'lança montada' | 'sarissa' | 'alabarda' | 'arco curto' |
 'arco' | 'arco longo' | 'besta' | 'besta pesada' | 'escudo redondo' | 'escudo' | 'scutum'
 
