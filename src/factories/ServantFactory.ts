fdf573dde17478456b4d35f0081157336d68f8e8
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-10-13 23:45:03 -0300

    Update ServantFactory.ts

--- src/factories/ServantFactory.ts
+++ src/factories/ServantFactory.ts
@@ -20,7 +20,7 @@ class Servant {
   inventory: Inventory
   maestry: Maestry
 
-  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, armor: Armor, currentWeapon: Weapon, attributes: Attributes = { agility: 0, technique: 0, strength: 0, fortitude: 0 }) {
+  constructor (id: string, masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, primaryArmor: Armor, secondaryArmor: Armor, primaryWeapon: Weapon, attributes: Attributes = { agility: 0, technique: 0, strength: 0, fortitude: 0 }) {
     this.id = id
     this.masterId = masterId
     this.name = name
@@ -31,7 +31,7 @@ class Servant {
     this.guard = 0
     this.buff = 0
     this.debuff = 0
-    this.inventory = { armor, carriedWeapons: [], currentWeapon, denars: 0 }
+    this.inventory = { primaryArmor, secondaryArmor, carriedWeapons: [], primaryWeapon, secondaryWeapon: null, denars: 0 }
     this.maestry = { bow: 0, crossbow: 0, bareHanded: 0, oneHanded: 0, twoHanded: 0, polearm: 0 }
   }
 }
@@ -40,7 +40,7 @@ class ServantFactory {
   constructor (private readonly uuidGenerator: UuidGenerator, private readonly armorFactory: ArmorFactory, private readonly weaponFactory: WeaponFactory) {}
 
   create (masterId: string, name: string, fatherProfession: Profession, youthProfession: Profession, attributes: Attributes): Servant {
-    return new Servant(this.uuidGenerator.generate(), masterId, name, fatherProfession, youthProfession, this.armorFactory.createArmorByFortitude(attributes.fortitude), this.weaponFactory.createWeapon('mão nua'), attributes)
+    return new Servant(this.uuidGenerator.generate(), masterId, name, fatherProfession, youthProfession, this.armorFactory.createArmorByType('roupa'), this.armorFactory.createArmorByType('roupa'), this.weaponFactory.createWeapon('mão nua'), attributes)
   }
 }
 
