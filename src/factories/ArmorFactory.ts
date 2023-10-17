6485cf2c57ad07cfc1ebac7265cc43a9b0d4590d
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-10-17 18:45:28 -0300

    Update ArmorFactory.ts

--- src/factories/ArmorFactory.ts
+++ src/factories/ArmorFactory.ts
@@ -3,18 +3,22 @@ import type ArmorType from '../bot/type/ArmorType'
 class Armor {
   readonly type: ArmorType
   readonly debuff: number
-  constructor (type: ArmorType, debuff: number) {
+  condition: number
+  readonly maximumCondition: number
+  constructor (type: ArmorType, debuff: number, condition: number) {
     this.type = type
     this.debuff = debuff
+    this.condition = condition
+    this.maximumCondition = condition
   }
 }
 
 class ArmorFactory {
   createArmorByType (armorType: ArmorType): Armor {
-    if (armorType === 'roupa') return new Armor(armorType, 0)
-    else if (armorType === 'couro') return new Armor(armorType, -2)
-    else if (armorType === 'cota de malha') return new Armor(armorType, -4)
-    else if (armorType === 'placa') return new Armor(armorType, -8)
+    if (armorType === 'roupa') return new Armor(armorType, 0, 0)
+    else if (armorType === 'couro') return new Armor(armorType, -2, 40)
+    else if (armorType === 'cota de malha') return new Armor(armorType, -4, 80)
+    else if (armorType === 'placa') return new Armor(armorType, -8, 120)
     else throw new Error('Tipo de armadura inválido')
   }
 }
