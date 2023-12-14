1d92b46e3a18b1be67f5c2ca43745b705d1a14c0
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-14 06:29:37 -0300

    feat: adding database servant type

---
+++ src/api/model/DatabaseServant.ts
@@ -0,0 +1,22 @@
+import type BattleInfo from '../../bot/type/BattleInfo'
+import type CombatCapabilities from '../../bot/type/CombatCapabilities'
+import type Inventory from '../../bot/type/Inventory'
+import type Maestry from '../../bot/type/Maestry'
+import Profession from '../../bot/type/Profession'
+import type Attributes from '../type/Attributes'
+
+interface DatabaseServant {
+  id: string
+  master_id: string
+  name: string
+  father_profession: Profession
+  youth_profession: Profession
+  current_attributes: Attributes
+  maximum_attributes: Attributes
+  combat_capabilities: CombatCapabilities
+  battle_info: BattleInfo
+  inventory: Inventory
+  maestry: Maestry
+}
+
+export default DatabaseServant
