7e883ac15aac6f24b5ae632d0a21e9ccf372531a
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-17 21:32:05 -0300

    refator: adding dto to battle repo

--- src/repository/BattleRepository.ts
+++ src/repository/BattleRepository.ts
@@ -1,26 +1,26 @@
 import type PostgresDataSource from '../data/PostgresDataSource'
-import type { Battle } from '../factories/BattleFactory'
+import type { BattleDTO } from '../factories/BattleFactory'
 
 class BattleRepository {
   constructor (readonly dataSource: PostgresDataSource) {}
 
-  async create (battle: Battle): Promise<Battle> {
+  async create (battle: BattleDTO): Promise<BattleDTO> {
     return await this.dataSource.insertBattleRegistry(battle)
   }
 
-  async getAll (): Promise<Battle[]> {
+  async getAll (): Promise<BattleDTO[]> {
     return await this.dataSource.fetchEveryBattleRegistry()
   }
 
-  async getByName (name: string): Promise<Battle | null> {
+  async getByName (name: string): Promise<BattleDTO | null> {
     return await this.dataSource.fetchBattleBy('name', name)
   }
 
-  async update (name: string, updatedBattle: Battle): Promise<Battle | null> {
+  async update (name: string, updatedBattle: BattleDTO): Promise<BattleDTO | null> {
     return await this.dataSource.updateBattleBy('name', name, updatedBattle)
   }
 
-  async delete (name: string): Promise<Battle | null> {
+  async delete (name: string): Promise<BattleDTO | null> {
     return await this.dataSource.deleteBattleBy('name', name)
   }
 }
