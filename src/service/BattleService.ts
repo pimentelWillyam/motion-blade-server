3fb56ad140a51caff70b3e74e184163aa2106f83
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-17 21:32:17 -0300

    refactor: adding dto to battle service

--- src/service/BattleService.ts
+++ src/service/BattleService.ts
@@ -1,10 +1,10 @@
-import type { Battle, BattleFactory } from '../factories/BattleFactory'
+import type { Battle, BattleDTO, BattleFactory } from '../factories/BattleFactory'
 import type BattleRepository from '../repository/BattleRepository'
 
 class BattleService {
   constructor (private readonly battleRepository: BattleRepository, private readonly battleFactory: BattleFactory) {}
 
-  create = async (battleName: string): Promise<Battle> => {
+  create = async (battleName: string): Promise<BattleDTO> => {
     if (!await this.battleExists(battleName)) {
       const battle = this.battleFactory.create(battleName)
       return await this.battleRepository.create(battle)
@@ -12,14 +12,19 @@ class BattleService {
     throw new Error('Já existe uma batalha com esse nome')
   }
 
-  getAll = async (): Promise<Battle[]> => {
+  getAll = async (): Promise<BattleDTO[]> => {
     return await this.battleRepository.getAll()
   }
 
   get = async (battleName: string): Promise<Battle> => {
     const fetchedBattle = await this.battleRepository.getByName(battleName)
-    if (fetchedBattle != null) return fetchedBattle
-    throw new Error(`a batalha ${battleName} não existe`)
+    if (fetchedBattle == null) throw new Error(`a batalha ${battleName} não existe`)
+    const battle = this.battleFactory.create(fetchedBattle.name)
+    battle.id = fetchedBattle.id
+    battle.participantsList = fetchedBattle.participantsList
+    battle.map = fetchedBattle.map
+    battle.turnInfo = fetchedBattle.turnInfo
+    return battle
   }
 
   battleExists = async (battleName: string): Promise<boolean> => {
@@ -28,13 +33,13 @@ class BattleService {
     return false
   }
 
-  update = async (battleName: string, contentToUpdate: Battle): Promise<Battle> => {
+  update = async (battleName: string, contentToUpdate: BattleDTO): Promise<BattleDTO> => {
     const battleToBeUpdated = await this.battleRepository.update(battleName, contentToUpdate)
     if (battleToBeUpdated != null) return battleToBeUpdated
     throw new Error('Não é possível atualizar um servo que não existe')
   }
 
-  delete = async (battleName: string): Promise<Battle> => {
+  delete = async (battleName: string): Promise<BattleDTO> => {
     const battleToBeDeleted = await this.battleRepository.delete(battleName)
     if (battleToBeDeleted != null) return battleToBeDeleted
     throw new Error('Não é possível remover uma batalha que não existe')
