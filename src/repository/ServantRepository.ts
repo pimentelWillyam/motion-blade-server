67f7255d1cf2f4fcdf973892dd0b1f154de0df20
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-02-20 19:29:55 -0300

    refactor: adding dto to many methods

--- src/repository/ServantRepository.ts
+++ src/repository/ServantRepository.ts
@@ -4,23 +4,23 @@ import type { PostgresDataSource } from '../data/PostgresDataSource'
 class ServantRepository {
   constructor (readonly dataSource: PostgresDataSource) {}
 
-  async create (servant: Servant): Promise<Servant> {
+  async create (servant: Servant): Promise<ServantDTO> {
     return await this.dataSource.insertServantRegistry(servant)
   }
 
-  async getAll (): Promise<Servant[]> {
+  async getAll (): Promise<ServantDTO[]> {
     return await this.dataSource.fetchEveryServantRegistry()
   }
 
-  async getByName (name: string): Promise<Servant | null> {
+  async getByName (name: string): Promise<ServantDTO | null> {
     return await this.dataSource.fetchServantBy('name', name)
   }
 
-  async update (name: string, updatedServant: Servant): Promise<Servant | null> {
+  async update (name: string, updatedServant: Servant): Promise<ServantDTO | null> {
     return await this.dataSource.updateServantBy('name', name, updatedServant)
   }
 
-  async delete (name: string): Promise<Servant | null> {
+  async delete (name: string): Promise<ServantDTO | null> {
     return await this.dataSource.deleteServantBy('name', name)
   }
 }
