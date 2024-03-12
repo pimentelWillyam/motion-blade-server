2f2dd0c418ff10c0783e5d4e087b730555b72e0e
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-03-12 16:20:53 -0300

    fix: fixing servant sorter bubblesort

--- src/bot/helper/ServantSorter.ts
+++ src/bot/helper/ServantSorter.ts
@@ -5,7 +5,7 @@ class ServantSorter implements IServantSorter {
   initiativeServantBubbleSort (servantArray: Servant[]): Servant[] {
     for (let i = 0; i < servantArray.length; i++) {
       for (let j = 0; j < (servantArray.length - i - 1); j++) {
-        if (servantArray[j].battlePoints.initiativePoints > servantArray[j + 1].battlePoints.initiativePoints) {
+        if (servantArray[j].battlePoints.initiativePoints < servantArray[j + 1].battlePoints.initiativePoints) {
           const temp = servantArray[j]
           servantArray[j] = servantArray[j + 1]
           servantArray[j + 1] = temp
