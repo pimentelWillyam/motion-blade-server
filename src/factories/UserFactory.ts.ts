b50c2f971b0f501dc80b829b50ec46385b986b8f
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-02 15:46:45 -0300

    feat: adding user

--- src/factories/UserFactory.ts.ts
+++ src/factories/UserFactory.ts.ts
@@ -1,3 +1,21 @@
+import { type UserType } from '../bot/type/UserType'
+
+class User {
+  readonly id: string
+  readonly login: string
+  readonly password: string
+  readonly type: UserType
+  readonly servantNameList: string[]
+
+  constructor (id: string, login: string, password: string, type: UserType) {
+    this.id = id
+    this.login = login
+    this.password = password
+    this.type = type
+    this.servantNameList = []
+  }
+}
+
 class UserFactory {
   createUser (id: string, login: string, password: string, type: UserType): User {
     return new User(id, login, password, type)
