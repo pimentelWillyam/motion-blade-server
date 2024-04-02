515541266b5063708047a30bee8eb3283fab1812
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-02 10:44:06 -0300

    fix: fixing participants name list property that was ill spelled

--- src/factories/BattleFactory.ts
+++ src/factories/BattleFactory.ts
@@ -8,37 +8,37 @@ import { type ServantSorter } from '../bot/helper/ServantSorter'
 interface BattleDTO {
   id: string
   name: string
-  participantsList: Servant[]
+  participantsNameList: string[]
   map: string[][]
   turnInfo: TurnInfo
 }
 class Battle {
   id: string
   name: string
-  participantsList: Servant[]
+  participantsNameList: string[]
   map: string[][]
   turnInfo: TurnInfo
 
-  constructor (private readonly servantSorter: ServantSorter, private readonly randomNumberGenerator: RandomNumberGenerator, id: string, name: string, participantsList: Servant[], map: string[][]) {
+  constructor (private readonly servantSorter: ServantSorter, private readonly randomNumberGenerator: RandomNumberGenerator, id: string, name: string, participantsNameList: string[], map: string[][]) {
     this.id = id
     this.name = name
-    this.participantsList = participantsList
+    this.participantsNameList = participantsNameList
     this.map = map
     this.turnInfo = { servantAboutToPlay: undefined, servantsYetToPlay: [] }
   }
 
   insertServant (servant: Servant): Servant {
     if (servant.battleInfo.isInBattle) throw new Error(`O servo ${servant.name} já está inserido em uma batalha`)
-    if (this.participantsList.length >= 10) throw new Error('Não é possível inserir mais que 10 servos em uma única batalha')
+    if (this.participantsNameList.length >= 10) throw new Error('Não é possível inserir mais que 10 servos em uma única batalha')
     let verticalPosition = this.randomNumberGenerator.generate(0, 7)
     let horizontalPosition = this.randomNumberGenerator.generate(0, 7)
     while (this.map[horizontalPosition][verticalPosition] !== '=') {
       verticalPosition = this.randomNumberGenerator.generate(0, 7)
       horizontalPosition = this.randomNumberGenerator.generate(0, 7)
     }
-    this.participantsList.push(servant)
+    this.participantsNameList.push(servant.name)
 
-    servant.battleInfo.battleId = this.participantsList.length
+    servant.battleInfo.battleId = this.participantsNameList.length
     servant.battleInfo.horizontalPosition = horizontalPosition
     servant.battleInfo.verticalPosition = verticalPosition
     servant.battleInfo.isInBattle = true
@@ -49,10 +49,10 @@ class Battle {
 
   removeServant (servant: Servant): void {
     if (servant.battleInfo.battleName !== this.name) throw new Error(`O servo ${servant.name} não pode ser removido pois não está nessa batalha`)
-    for (let i = 0; i < this.participantsList.length; i++) {
-      if (servant.name === this.participantsList[i].name) {
+    for (let i = 0; i < this.participantsNameList.length; i++) {
+      if (servant.name === this.participantsNameList[i]) {
         this.map[servant.battleInfo.verticalPosition][servant.battleInfo.horizontalPosition] = '='
-        this.participantsList.splice(i, 1)
+        this.participantsNameList.splice(i, 1)
         return
       }
     }
@@ -153,8 +153,8 @@ class Battle {
     return servant
   }
 
-  getServantsInBattleOrderedByInitiative (): Servant[] {
-    return this.servantSorter.initiativeServantBubbleSort(this.participantsList)
+  getServantsInBattleOrderedByInitiative (servantList: Servant[]): Servant[] {
+    return this.servantSorter.initiativeServantBubbleSort(servantList)
   }
 }
 
