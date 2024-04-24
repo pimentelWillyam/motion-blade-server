3943b71bfff7f1fb3c80fe69453a0d2f8dcd107c
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-24 16:38:07 -0300

    feat: adding master router to api

--- src/helper/Api.ts
+++ src/helper/Api.ts
@@ -5,14 +5,16 @@ import * as cors from 'cors'
 import { json } from 'express'
 import type ServantRouter from '../api/router/ServantRouter'
 import type BattleRouter from '../api/router/BattleRouter'
+import type MasterRouter from '../api/router/MasterRouter'
 
 class Api {
-  constructor (readonly server: Express, readonly servantRouter: ServantRouter, readonly battleRouter: BattleRouter) {
+  constructor (readonly server: Express, readonly servantRouter: ServantRouter, readonly battleRouter: BattleRouter, readonly masterRouter: MasterRouter) {
     this.server.use(bodyParser.json())
     this.server.use(json())
     this.server.use(cors())
     this.server.use('/api', servantRouter.routes)
     this.server.use('/api', battleRouter.routes)
+    this.server.use('/api', masterRouter.routes)
   }
 }
 
