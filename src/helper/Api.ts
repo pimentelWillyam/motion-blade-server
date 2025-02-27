648a486851b87ba3c35c273b990be5d83e5623bd
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-02-27 20:43:32 -0300

    feat: adding user router to the api

--- src/helper/Api.ts
+++ src/helper/Api.ts
@@ -6,15 +6,17 @@ import { json } from 'express'
 import type ServantRouter from '../api/router/ServantRouter'
 import type BattleRouter from '../api/router/BattleRouter'
 import type MasterRouter from '../api/router/MasterRouter'
+import type UserRouter from '../api/router/UserRouter'
 
 class Api {
-  constructor (readonly server: Express, readonly servantRouter: ServantRouter, readonly battleRouter: BattleRouter, readonly masterRouter: MasterRouter) {
+  constructor (readonly server: Express, readonly servantRouter: ServantRouter, readonly battleRouter: BattleRouter, readonly masterRouter: MasterRouter, readonly userRouter: UserRouter) {
     this.server.use(bodyParser.json())
     this.server.use(json())
     this.server.use(cors())
     this.server.use('/api', servantRouter.routes)
     this.server.use('/api', battleRouter.routes)
     this.server.use('/api', masterRouter.routes)
+    this.server.use('/api', userRouter.routes)
   }
 }
 
