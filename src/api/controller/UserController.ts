a5ae0d9c2fbb7aba6c8d981d8a88de934eb83d84
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-02-27 20:42:23 -0300

    feat: adicionando controller do usuario

---
+++ src/api/controller/UserController.ts
@@ -0,0 +1,71 @@
+import { type Request, type Response } from 'express'
+
+import type UserValidator from '../validator/UserValidator'
+import type UserService from '../../service/UserService'
+
+enum UserError {
+  USER_INVALID_REQUEST = 'A requisição inserida foi considerada inválida',
+  USER_LIST_ERROR = 'Houve um erro quando tentamos buscar a lista',
+  USER_NOT_FOUND = 'Não foi possível encontrar este usuário',
+  USER_NOT_UPDATED = 'Não foi possível atualizar este usuário',
+  USER_NOT_DELETED = 'Não foi possível deletar este usuário',
+}
+class UserController {
+  constructor (readonly userService: UserService, readonly userValidator: UserValidator) {}
+
+  async create (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    try {
+      if (this.userValidator.isUserValid('need to test better')) {
+        if (req.body.login === undefined || req.body.password === undefined) {
+          const user = await this.userService.create(req.body.login, req.body.password)
+          return res.status(200).json(user)
+        }
+        const user = await this.userService.create(req.body.login, req.body.password)
+        return res.status(200).json(user)
+      }
+    } catch (erro) {
+      console.error(erro)
+    }
+    return res.status(400).send(UserError.USER_INVALID_REQUEST)
+  }
+
+  async getAll (res: Response): Promise<Response<any, Record<string, any>>> {
+    try {
+      const userList = await this.userService.getAll()
+      return res.status(200).json(userList)
+    } catch (erro) {
+      console.error(erro)
+    }
+    return res.status(400).send(UserError.USER_INVALID_REQUEST)
+  }
+
+  async get (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    try {
+      const user = await this.userService.get(req.params.login)
+      if (user != null) {
+        return res.status(200).json(user)
+      } else {
+        return res.status(404).send(UserError.USER_NOT_FOUND)
+      }
+    } catch (erro) {
+      console.error(erro)
+    }
+    return res.status(400).send(UserError.USER_INVALID_REQUEST)
+  }
+
+  async delete (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    try {
+      const user = await this.userService.delete(req.params.login)
+      if (user != null) {
+        return res.status(200).json(user)
+      } else {
+        return res.status(404).send(UserError.USER_NOT_FOUND)
+      }
+    } catch (erro) {
+      console.error(erro)
+    }
+    return res.status(400).send(UserError.USER_INVALID_REQUEST)
+  }
+}
+
+export default UserController
