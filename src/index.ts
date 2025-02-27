52ccde29682df02bce40bf1b5abc4ad33c1ee843
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-02-27 20:43:25 -0300

    feat: importing user related classes

--- src/index.ts
+++ src/index.ts
@@ -71,6 +71,13 @@ import MasterService from './service/MasterService'
 import { MasterFactory } from './factories/MasterFactory'
 import MasterValidator from './api/validator/MasterValidator'
 
+import UserRouter from './api/router/UserRouter'
+import UserController from './api/controller/UserController'
+import UserService from './service/UserService'
+import UserRepository from './repository/UserRepository'
+import UserValidator from './api/validator/UserValidator'
+import { UserFactory } from './factories/UserFactory'
+
 // instanciating uuid generator
 const uuidGenerator = new UuidGenerator()
 
@@ -94,27 +101,32 @@ const weaponFactory = new WeaponFactory()
 const servantFactory = new ServantFactory(randomNumberGenerator, uuidGenerator, armorFactory, weaponFactory)
 const battleFactory = new BattleFactory(new ServantSorter(), randomNumberGenerator, uuidGenerator)
 const masterFactory = new MasterFactory(uuidGenerator)
+const userFactory = new UserFactory(uuidGenerator)
 
 // instanciating repository
 const servantRepository = new ServantRepository(postgresDataSource)
 const battleRepository = new BattleRepository(postgresDataSource)
 const masterRepository = new MasterRepository(postgresDataSource)
+const userRepository = new UserRepository(postgresDataSource)
 
 // instanciating services
 
 const servantService = new ServantService(servantRepository, attributesFetcher, servantFactory, armorFactory, weaponFactory)
 const battleService = new BattleService(battleRepository, battleFactory)
 const masterService = new MasterService(masterRepository, masterFactory)
+const userService = new UserService(userRepository, userFactory)
 
 // instanciating validators
 const servantValidator = new ServantValidator()
 const battleValidator = new BattleValidator()
 const masterValidator = new MasterValidator()
+const userValidator = new UserValidator()
 
 // instanciating the servant controller
 const servantController = new ServantController(servantService, servantValidator)
 const battleController = new BattleController(battleService, battleValidator)
 const masterController = new MasterController(masterService, masterValidator)
+const userController = new UserController(userService, userValidator)
 
 // instanciating the command manager
 const commandManager = new CommandManager(new ServantSorter(), randomNumberGenerator, new Sleeper(), servantService, battleService, new ServantUpgrader(randomNumberGenerator), new CombatManager(servantService, new ServantUpgrader(randomNumberGenerator), randomNumberGenerator))
@@ -132,9 +144,10 @@ const server = new Server()
 const servantRouter = new ServantRouter(servantController)
 const battleRouter = new BattleRouter(battleController)
 const masterRouter = new MasterRouter(masterController)
+const userRouter = new UserRouter(userController)
 
 // instanciating app related classes
-const api = new Api(express(), servantRouter, battleRouter, masterRouter)
+const api = new Api(express(), servantRouter, battleRouter, masterRouter, userRouter)
 const app = new App(api, server)
 
 // getting .env configuration
