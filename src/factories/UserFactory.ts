0368f4a0c8c1835dceef90c8822d93292943a8e0
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-02-27 20:42:56 -0300

    feat: adding user factory

---
+++ src/factories/UserFactory.ts
@@ -0,0 +1,48 @@
+import type UuidGenerator from '../bot/helper/UuidGenerator'
+import { type Servant } from './ServantFactory'
+import { type Battle } from './BattleFactory'
+
+interface UserDTO {
+  readonly id: string
+  readonly login: string
+  readonly password: string
+  readonly servantList: Servant[]
+  readonly battleList: Battle[]
+}
+
+class User {
+  readonly id: string
+  readonly login: string
+  readonly password: string
+  readonly servantList: Servant[]
+  readonly battleList: Battle[]
+
+  constructor (user: UserDTO) {
+    this.id = user.id
+    this.login = user.login
+    this.password = user.password
+    this.servantList = user.servantList
+    this.battleList = user.battleList
+  }
+}
+
+class UserFactory {
+  constructor (private readonly uuidGenerator: UuidGenerator) {}
+
+  create (login: string, password: string): User {
+    const user: UserDTO = {
+      id: this.uuidGenerator.generate(),
+      login,
+      password,
+      servantList: [],
+      battleList: []
+    }
+    return new User(user)
+  }
+
+  createThroughDto (user: UserDTO): User {
+    return new User(user)
+  }
+}
+
+export { UserFactory, User, type UserDTO }
