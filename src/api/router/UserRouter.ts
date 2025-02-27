5799d0035be2b487f80de4a351d2f1157a73884d
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-02-27 20:42:39 -0300

    feat: adding user router

---
+++ src/api/router/UserRouter.ts
@@ -0,0 +1,29 @@
+// importando core da rota
+import * as express from 'express'
+import { type Router, type Request, type Response } from 'express'
+import type UserController from '../controller/UserController'
+
+// importando service da rota
+
+// criando rotas
+
+class UserRouter {
+  readonly routes: Router
+  constructor (readonly userController: UserController) {
+    this.routes = express.Router()
+    this.routes.post('/user', (req: Request, res: Response) => {
+      void userController.create(req, res)
+    })
+    this.routes.get('/user', (req: Request, res: Response) => {
+      void userController.getAll(res)
+    })
+    this.routes.get('/user/:login', (req: Request, res: Response) => {
+      void userController.get(req, res)
+    })
+    this.routes.delete('/user/:login', (req: Request, res: Response) => {
+      void userController.delete(req, res)
+    })
+  }
+}
+
+export default UserRouter
