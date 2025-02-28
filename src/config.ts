dac2dd964df6cecbfcd43597a41c7b9a9e0cd811
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-02-28 16:36:56 -0300

    refactor: modificando porta da api

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ const config = {
   },
   api: {
     host: process.env.HOST_API ?? 'localhost',
-    port: process.env.PORT_API ?? 4000
+    port: process.env.PORT_API ?? 4001
   },
   mariadb: {
     type: 'mysql',
