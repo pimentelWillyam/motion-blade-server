3736972b6e53f0b78b2debcacd48053eed4eabbd
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-02 08:16:50 -0300

    feat: adding token manager

--- src/helper/TokenManager.ts
+++ src/helper/TokenManager.ts
@@ -1,9 +1,8 @@
-import type ITokenManager from '../api/interface/ITokenManager'
 
 import * as jsonwebtoken from 'jsonwebtoken'
 import config from '../config'
 
-class TokenManager implements ITokenManager {
+class TokenManager {
   generate (id: string): string {
     return jsonwebtoken.sign({ id }, config.json.secret, { expiresIn: '12h' })
   }
