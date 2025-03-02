62bc6f1965d93b8ef1c4b1fdd83cb42c6bb57489
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-02 08:16:24 -0300

    feat: adicionando rota de autenticacao

--- src/helper/Api.ts
+++ src/helper/Api.ts
@@ -7,9 +7,10 @@ import type ServantRouter from '../api/router/ServantRouter'
 import type BattleRouter from '../api/router/BattleRouter'
 import type MasterRouter from '../api/router/MasterRouter'
 import type UserRouter from '../api/router/UserRouter'
+import AuthRouter from '../api/router/AuthRouter'
 
 class Api {
-  constructor (readonly server: Express, readonly servantRouter: ServantRouter, readonly battleRouter: BattleRouter, readonly masterRouter: MasterRouter, readonly userRouter: UserRouter) {
+  constructor (readonly server: Express, readonly servantRouter: ServantRouter, readonly battleRouter: BattleRouter, readonly masterRouter: MasterRouter, readonly userRouter: UserRouter, readonly authRouter: AuthRouter) {
     this.server.use(bodyParser.json())
     this.server.use(json())
     this.server.use(cors())
@@ -17,6 +18,8 @@ class Api {
     this.server.use('/api', battleRouter.routes)
     this.server.use('/api', masterRouter.routes)
     this.server.use('/api', userRouter.routes)
+    this.server.use('/api', authRouter.routes)
+
   }
 }
 
